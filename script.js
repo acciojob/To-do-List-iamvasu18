ade63abe72df9f0c99273866a54c821247461531
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-25 23:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+const input=document.getElementById("newTodoInput");
+const button=document.getElementById("addTodoBtn");
+const ol=document.getElementById("todoList");
 
+button.addEventListener("click",=>(){
+	const text = input.value.trim();
+    if (text === "") {
+        alert("Input is empty");
+        return;
+    }
+	const list=document.createElement("li");
+	list.textContent=text;
+    ol.appendChild(list)
+})
